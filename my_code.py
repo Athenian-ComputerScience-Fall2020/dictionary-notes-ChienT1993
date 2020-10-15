035f78b6d8a18a8ad936b88a365c96ce391b640b
ChienT1993 <21ctran@athenian.org>
2020-10-15 23:44:08 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -1,2 +1,25 @@
 # Use this to take notes on the Edpuzzle video. Try each example rather than just watching it - you will get much more out of it!
 #  
+me = {'name': 'Chien', 'age': '17', 'class': 'senior'}
+
+
+#print (me['name'])
+#print (me.get('phone', 'not found'))#not available key
+
+
+#me['phone'] = '999-9999'#add
+#me['age'] = 'seventeen'#add
+#or me.update ({'name': 'Tran', 'age': '16', 'phone': '999-999'})#add
+
+
+#del me['age']#delete
+#age = me.pop ('age')#delete from list to use seperately
+
+
+#print (me.keys())
+#print (me.values())
+#print (me.items())#print both keys and values
+
+
+#for key, value in me.items():
+    #print (key, value)
